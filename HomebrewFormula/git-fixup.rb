88391b3c132e09dadeb6572c801b235df32a6e59
Brandon W Maister <quodlibetor@gmail.com>
2018-06-05 20:58:29 -0400

    Update version to 0.1.4
    0.1.1 was busted on macos.

--- HomebrewFormula/git-fixup.rb
+++ HomebrewFormula/git-fixup.rb
@@ -1,16 +1,16 @@
 class GitFixup < Formula
-  version "0.1.1"
+  version "0.1.4"
   desc "Fixup old git commits"
   homepage "https://github.com/quodlibetor/git-fixup"
 
   if OS.mac?
-      depends_on "openssl@1.1"
+      depends_on "openssl"
       depends_on "libssh2"
       url "https://github.com/quodlibetor/git-fixup/releases/download/v#{version}/git-fixup-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "ce83c04e076596e378074a273bb3574c7e3530be8ce030412b7ccdafb6b59772"
+      sha256 "95587cc3a2b51063e2d6cb9b6a3cb9b5a1e1e17ea77db7d1b31d7e0f4fd722b1"
   elsif OS.linux?
       url "https://github.com/quodlibetor/git-fixup/releases/download/v#{version}/git-fixup-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c69f47be2f420905e1e2eff9f0bcc794d34a6c1aa8f67cf3c77c1d4136ea6e5b"
+      sha256 "c51d1d832c208bd375bfd2575a6eaea15b5e620d12718baab793db1552f6eedb"
   end
 
   def install

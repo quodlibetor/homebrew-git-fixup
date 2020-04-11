3459b585175de5973ae51fbc848f6ef6a923202d
Brandon W Maister <quodlibetor@gmail.com>
2020-04-11 13:50:06 -0400

    Update to 0.1.8

--- HomebrewFormula/git-fixup.rb
+++ HomebrewFormula/git-fixup.rb
@@ -1,16 +1,14 @@
 class GitFixup < Formula
-  version "0.1.5"
+  version "0.1.8"
   desc "Fixup old git commits"
   homepage "https://github.com/quodlibetor/git-fixup"
 
   if OS.mac?
-      depends_on "openssl"
-      depends_on "libssh2"
       url "https://github.com/quodlibetor/git-fixup/releases/download/v#{version}/git-fixup-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "b346aca1bfef57c992a1995af413bd7c52aeef36fb7d57972f9500488cf5cb8f"
+      sha256 "aa0213a8569101cfe3a52719f2294b499e69adca869970c145b90d11a939575e"
   elsif OS.linux?
-      url "https://github.com/quodlibetor/git-fixup/releases/download/v#{version}/git-fixup-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "48430a02558562540eb2ba45b2038a9de7fee63ea743340dd9c56d489fcaf61e"
+      url "https://github.com/quodlibetor/git-fixup/releases/download/v#{version}/git-fixup-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "42e8249f19b2685729f1d3292b99500868aa375f034d9aa289e435b125edcb2b"
   end
 
   def install

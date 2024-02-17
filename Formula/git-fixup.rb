112f9a3966703ce68de9e159e6e8d678b6904219
Brandon W Maister <quodlibetor@gmail.com>
2024-02-17 17:40:08 -0500

    Deprecate git-fixup in favor of git-instafix

--- Formula/git-fixup.rb
+++ Formula/git-fixup.rb
@@ -19,6 +19,8 @@ class GitFixup < Formula
     end
   end
 
+  deprecate! date: "2024-02-17", because: "Renamed to git-instafix. Install quodlibetor/git-tools/git-instafix instead"
+
   def install
     on_macos do
       on_arm do
